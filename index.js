83260e05229512919b2de0e56070a62a91d3e732
Fanessa Hilaire <fan@Fanessas-MacBook-Air.local>
2022-10-20 11:05:51 -0400

    add furniture

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import express from 'express'
+import { addNewFurniture } from './furniture.js'
+
 
 const app = express()
 app.use(express.json())
+let PORT = 3030
 
-app.get('/',(req,res ) => res.send)('working')
-
+app.get('/',(req,res ) => res.send('working'))
+app.post('/furniture', addNewFurniture);
 
 
 
